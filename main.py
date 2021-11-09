a613fc731ed2ec2423d18e9fb8343326a7ae0dfe
leht377 <migithub2019@gmail.com>
2021-11-08 20:48:28 -0500

    Se agrego la implementacion de las session

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session
 import os
+from flask.helpers import url_for
 from flask_wtf import form
 from werkzeug.security import check_password_hash, generate_password_hash
 from formularios import form_loginSGE, form_search
@@ -24,11 +25,11 @@ def inicio():
     form = form_loginSGE()
     return render_template('login.html', form=form)
 
-
-@app.route('/Dashboard', methods=["GET", "POST"])
-def dashboard():
-    if request.method == 'POST':
+@app.route('/ingreso', methods = ["POST"]) #aca se valida el usuario
+def ingreso():
+     if request.method == 'POST':
         global informacion
+
         form = form_loginSGE()
         User = form.Usuario.data
         Passw = form.contrasena.data
@@ -38,41 +39,63 @@ def dashboard():
         id = cur.fetchone()
 
         if id != None:
+            session['Usuario'] = True
             cur.execute("SELECT empleado.cedula, empleado.nombre,empleado.apellido,empleado.rol_id,rol.rol FROM empleado,rol WHERE empleado.cedula = %s and empleado.rol_id = rol.id;", (id))
             informacion = cur.fetchall()[0]
             cur.close()
             mysql.connection.commit()
-            form = form_search()
-            return render_template('baseDashboard.html', form=form, informacion=informacion)
+            return redirect('/Dashboard')
+        else:
+            return redirect('/')
+
+@app.route('/Dashboard', methods=["GET", "POST"])
+def dashboard():
+    if "Usuario" in session:
+        global informacion
+        form = form_search()
+        return render_template('baseDashboard.html', form=form, informacion=informacion)
     else:
-        redirect('/')
-
-# POR HACER
-
+        return redirect('/')
 
 @app.route('/Dashboard/informacion_personal', methods=["GET", "POST"])
 def informacionPersonal():
-    global informacion
-    form = form_search()
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT empleado.cedula,empleado.nombre,empleado.apellido,empleado.telefono,empleado.salario,empleado.dependencia ,datos_de_usuario.usuario, informacioncontrato.fecha_ingreso,informacioncontrato.fecha_terminacion,informacioncontrato.tipoContrato,rol.rol FROM empleado,informacioncontrato,datos_de_usuario,rol WHERE empleado.cedula = %s and empleado.datos_de_usuario_id =  datos_de_usuario.id and informacioncontrato.empleado_cedula = empleado.cedula and empleado.rol_id = rol.id;",(informacion[0],))
-    datosUsuario = cur.fetchall()[0]
-    return render_template('includes/informacionPersonal.html', form=form, informacion=informacion,datosUsuario=datosUsuario)
-
+    if "Usuario" in session:
+        global informacion
+        form = form_search()
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT empleado.cedula,empleado.nombre,empleado.apellido,empleado.telefono,empleado.salario,empleado.dependencia ,datos_de_usuario.usuario, informacioncontrato.fecha_ingreso,informacioncontrato.fecha_terminacion,informacioncontrato.tipoContrato,rol.rol FROM empleado,informacioncontrato,datos_de_usuario,rol WHERE empleado.cedula = %s and empleado.datos_de_usuario_id =  datos_de_usuario.id and informacioncontrato.empleado_cedula = empleado.cedula and empleado.rol_id = rol.id;",(informacion[0],))
+        datosUsuario = cur.fetchall()[0]
+        return render_template('includes/informacionPersonal.html', form=form, informacion=informacion,datosUsuario=datosUsuario)
+    else:
+        return redirect('/')
+# POR HACER
 
 @app.route('/Dashboard/ver_retroalimentacion', methods=["GET", "POST"])
 def ver_retroalimentacion():
-    global informacion
-    form = form_search()
-    return render_template('includes/verRetroalimentacion.html', form=form, informacion=informacion)
+    if "Usuario" in session:
+        global informacion
+        form = form_search()
+        return render_template('includes/verRetroalimentacion.html', form=form, informacion=informacion)
+    else:
+        return redirect('/')
 
 
 @app.route('/Dashboard/lista_empleados', methods=["GET", "POST"])
 def listar_empleados():
-    global informacion
-    form = form_search()
-    return render_template('includes/listarEmpleados.html', form=form, informacion=informacion)
+    if "Usuario" in session:
+        global informacion
+        form = form_search()
+        return render_template('includes/listarEmpleados.html', form=form, informacion=informacion)
+    else:
+        return redirect('/')
 
-
+@app.route('/logout')
+def logout():
+    if "Usuario" in session:
+        session.pop("Usuario",None)
+        return redirect('/')
+    else:
+        return redirect('/')
+        
 if __name__ == "__main__":
     app.run(debug=True)
